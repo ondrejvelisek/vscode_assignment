dfc5d49dc5d03571b908b922da98c11867dbbf1d
Ondrej Velisek <ondrejvelisek@gmail.com>
2023-12-28 16:56:18 +0100

    Add possibility to abort service E

--- service-compositor/src/createBECFPath.ts
+++ service-compositor/src/createBECFPath.ts
@@ -3,11 +3,14 @@ import { promisifyB } from "./utils";
 
 export function createBECFPath (b: ServiceB, e: ServiceE, c: ServiceC, f: ServiceF, errorMapper: ErrorMapper) {
     return async (req: Main_Request, timeoutMillis: number, cancellation: CancellationToken): Promise<Main_Result> => {
-        const resB = await promisifyB(b, errorMapper)(req, timeoutMillis, cancellation);
-        const [resE1Promise, cancelTokenE1] = e.transform(Promise.resolve(resB))
-        const resE1 = await resE1Promise;
-        const resC = await c.call(req);
-        const resF1 = f.present(resC, resE1);
-        return resF1;
+        const resB = promisifyB(b, errorMapper)(req, timeoutMillis, cancellation);
+
+        const [resEPromise, cancelTokenE] = e.transform(resB)
+        cancellation.onCancelled(cancelTokenE);
+
+        const [resC, resE] = await Promise.all([c.call(req), resEPromise]);
+
+        const resF = f.present(resC, resE);
+        return resF;
     }
 }
